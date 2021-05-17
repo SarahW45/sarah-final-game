877554260364cf9763ef8fc9b4a80f78840aba75
SarahW45 <82471934+SarahW45@users.noreply.github.com>
2021-05-17 15:49:16 -0400

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,25 +3,6 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "WZ~W{3X=75s(JqilkpuE":
-            case "ducky":return img`
-. . . . . . . . . b 5 b . . . . 
-. . . . . . . . . b 5 b . . . . 
-. . . . . . b b b b b b . . . . 
-. . . . . b b 5 5 5 5 5 b . . . 
-. . . . b b 5 b c 5 5 d 4 c . . 
-. b b b b 5 5 5 b f d d 4 4 4 b 
-. b d 5 b 5 5 b c b 4 4 4 4 b . 
-. . b 5 5 b 5 5 5 4 4 4 4 b . . 
-. . b d 5 5 b 5 5 5 5 5 5 b . . 
-. b d b 5 5 5 d 5 5 5 5 5 5 b . 
-b d d c d 5 5 b 5 5 5 5 5 5 b . 
-c d d d c c b 5 5 5 5 5 5 5 b . 
-c b d d d d d 5 5 5 5 5 5 5 b . 
-. c d d d d d d 5 5 5 5 5 d b . 
-. . c b d d d d d 5 5 5 b b . . 
-. . . c c c c c c c c b b . . . 
-`;
             case "[jG{V8}qJsHKbdZgVWz+":
             case "weapon":return img`
 . . . . . . . . . . . . . . . . 
@@ -40,6 +21,25 @@ c b d d d d d 5 5 5 5 5 5 5 b .
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
+`;
+            case "WZ~W{3X=75s(JqilkpuE":
+            case "ducky":return img`
+. . . . . a a a a a a a a a . . 
+. . . . a a a a a a a a a a a . 
+. . . a a a a a a a a a a a a a 
+. . . a a a a a a a a a a a a a 
+. . . a a f f f f f f f f a a a 
+. b b a f f f f f f f f f f f a 
+. b d a f f f f f f f f f f a a 
+. . b 5 a a a a a a a a a a a . 
+. . b d 5 a a a a a a a a a . . 
+. b d b 5 5 5 d 5 5 5 5 5 5 b . 
+b d d c d 5 5 b 5 5 5 5 5 5 b . 
+c d d d c c b 5 5 5 5 5 5 5 b . 
+c b d d d d d 5 5 5 5 5 5 5 b . 
+. c d d d d d d 5 5 5 5 5 d b . 
+. . c b d d d d d 5 5 5 b b . . 
+. . . c c c c c c c c b b . . . 
 `;
         }
         return null;
--- main.ts
+++ main.ts
@@ -4,13 +4,16 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     otherSprite.destroy()
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+	
+})
 let projectile: Sprite = null
 let ducky: Sprite = null
 scene.setBackgroundColor(15)
 ducky = sprites.create(assets.image`ducky`, SpriteKind.Player)
 ducky.setPosition(80, 101)
 controller.moveSprite(ducky)
-let Alien = sprites.create(img`
+let Alien = sprites.createProjectileFromSide(img`
     . . . . c c c c c c c . . . . . 
     . . . c c c c c c c c c . . . . 
     . . a c a c a c a c a c a . . . 
@@ -27,4 +30,5 @@ let Alien = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Enemy)
+    `, 20, 20)
+Alien.setKind(SpriteKind.Enemy)
