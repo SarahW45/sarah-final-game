580e5324ce3839aab05c9a43b094195ab701ad34
SarahW45 <82471934+SarahW45@users.noreply.github.com>
2021-05-18 15:45:45 -0400

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -24,22 +24,22 @@ namespace myImages {
 `;
             case "WZ~W{3X=75s(JqilkpuE":
             case "ducky":return img`
-. . . . . a a a a a a a a a . . 
-. . . . a a a a a a a a a a a . 
-. . . a a a a a a a a a a a a a 
-. . . a a a a a a a a a a a a a 
-. . . a a f f f f f f f f a a a 
-. b b a f f f f f f f f f f f a 
-. b d a f f f f f f f f f f a a 
-. . b 5 a a a a a a a a a a a . 
-. . b d 5 a a a a a a a a a . . 
-. b d b 5 5 5 d 5 5 5 5 5 5 b . 
-b d d c d 5 5 b 5 5 5 5 5 5 b . 
-c d d d c c b 5 5 5 5 5 5 5 b . 
-c b d d d d d 5 5 5 5 5 5 5 b . 
-. c d d d d d d 5 5 5 5 5 d b . 
-. . c b d d d d d 5 5 5 b b . . 
-. . . c c c c c c c c b b . . . 
+. . . . . . . . . . b 5 b . . . 
+. . . . . . . . . b 5 b . . . . 
+. . . . . . b b b b b b . . . . 
+. . . . . b b 5 5 5 5 5 b . . . 
+. . . . b b 5 d 1 f 5 d 4 c . . 
+. . . . b 5 5 1 f f d d 4 4 4 b 
+. . . . b 5 5 d f b 4 4 4 4 b . 
+. . . b d 5 5 5 5 4 4 4 4 b . . 
+. b b d d d 5 5 5 5 5 5 5 b . . 
+b d d d b b b 5 5 5 5 5 5 5 b . 
+c d d b 5 5 d c 5 5 5 5 5 5 b . 
+c b b d 5 d c d 5 5 5 5 5 5 b . 
+c b 5 5 b c d d 5 5 5 5 5 5 b . 
+b b c c c d d d 5 5 5 5 5 d b . 
+. . . . c c d d d 5 5 5 b b . . 
+. . . . . . c c c c c b b . . . 
 `;
         }
         return null;
--- main.ts
+++ main.ts
@@ -1,19 +1,21 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile = sprites.createProjectileFromSprite(assets.image`weapon`, ducky, 0, -50)
 })
+info.onLifeZero(function () {
+    game.over(false)
+})
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     otherSprite.destroy()
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-	
-})
 let projectile: Sprite = null
+let Alien: Sprite = null
 let ducky: Sprite = null
+info.setLife(4)
 scene.setBackgroundColor(15)
 ducky = sprites.create(assets.image`ducky`, SpriteKind.Player)
 ducky.setPosition(80, 101)
 controller.moveSprite(ducky)
-let Alien = sprites.createProjectileFromSide(img`
+Alien = sprites.createProjectileFromSprite(img`
     . . . . c c c c c c c . . . . . 
     . . . c c c c c c c c c . . . . 
     . . a c a c a c a c a c a . . . 
@@ -30,5 +32,29 @@ let Alien = sprites.createProjectileFromSide(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    `, 20, 20)
+    `, Alien, 31, 35)
 Alien.setKind(SpriteKind.Enemy)
+ducky.setStayInScreen(true)
+game.onUpdateInterval(2000, function () {
+    Alien.setBounceOnWall(true)
+    Alien.setStayInScreen(true)
+    Alien = sprites.createProjectileFromSide(img`
+        . . . . c c c c c c c . . . . . 
+        . . . c c c c c c c c c . . . . 
+        . . a c a c a c a c a c a . . . 
+        . c c c c c c c c c c c c c . . 
+        . c c 1 1 1 c c 1 1 1 c c c . . 
+        . c c c c c c c c c c c c c . . 
+        . c c c c c c c c c c c c c . . 
+        . c c c c 1 1 1 1 c c c c c . . 
+        . c c c c 1 1 1 1 c c c c c . . 
+        . c c c c c c c c c c c c c . . 
+        . . c c c c c c c c c c c . . . 
+        . . . c c c c c c c c c . . . . 
+        . . . . c c c c c c c . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, randint(30, 100), 6)
+    Alien.setKind(SpriteKind.Enemy)
+})
